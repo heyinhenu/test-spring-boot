c86a6c499538910d8ae7150bdb6cd613bc50265f
itang <live.tang@gmail.com>
2015-05-03 17:51:59 +0800

    cleanup

--- src/main/scala/demo/ext/ScalaBaseController.scala
+++ src/main/scala/demo/ext/ScalaBaseController.scala
@@ -6,15 +6,15 @@ import org.springframework.web.servlet.ModelAndView
 
 abstract class ScalaBaseController extends BaseController {
 
+  protected def view(path: String): ModelAndView = {
+    new ModelAndView(path)
+  }
+
   protected def view(path: String, context: (String, AnyRef)*): ModelAndView = {
-    new ModelAndView(path, Map(context: _*).asJava)
+    view(path, Map(context: _*))
   }
 
   protected def view(path: String, context: Map[String, AnyRef]): ModelAndView = {
     new ModelAndView(path, context.asJava)
-  }
-
-  protected def view(path: String): ModelAndView = {
-    new ModelAndView(path)
   }
 }
