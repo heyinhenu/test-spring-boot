d465424a8e9fc78ee40f33a996c2d8c764938ab1
itang <live.tang@gmail.com>
2015-05-14 17:05:10 +0800

    cleanup

--- src/main/scala/demo/ext/App.scala
+++ src/main/scala/demo/ext/App.scala
@@ -6,7 +6,7 @@ trait App {
 
 object SpringApp extends App with Logable {
 
-  val runMode: RunMode = {
+  override val runMode: RunMode = {
     val r = RunMode.fromString(System.getProperty(RunMode.RUN_MODE_KEY))
     if (!r.isPresent) {
       logger.warn(s"未设置${RunMode.RUN_MODE_KEY}属性, 默认为RunMode为: ${RunMode.Dev}")
